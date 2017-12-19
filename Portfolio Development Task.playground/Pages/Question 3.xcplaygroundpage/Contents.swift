d71db5dd497b80f1952bc631d96312238f496edf
lcs-tdauletov <32198299+lcs-tdauletov@users.noreply.github.com>
2017-12-19 09:36:55 -0500

    Done with the third page

--- Portfolio Development Task.playground/Pages/Question 3.xcplaygroundpage/Contents.swift
+++ Portfolio Development Task.playground/Pages/Question 3.xcplaygroundpage/Contents.swift
@@ -21,23 +21,24 @@
  
  */
 // Answer question 3 below
+let hairDonationAmount = 55.75
+print("The type inference selects the Double type, and I'm entirely solidar with it, because the initial amount has decimal points")
+let runTrainingDonationAmount = 15.35
+print("Again, the type inference selects the Double type, and I'm entirely solidar with it, because the initial amount has decimal points, too.")
+var headShavedCount = 12
+print("The number of heads has to stay an integer, because we're not discussing a situation where we have a fraction of a head. I'm using a type inference to assign an Int type.")
+var runTrainingCount = 73
+print("The number of students has to stay an integer, because in this case the other types are not applicable, as we're counting integrated students, not their parts.")
+var totalDonationAmount = (hairDonationAmount * Double(headShavedCount)) + (runTrainingDonationAmount * Double(runTrainingCount))
+print("All the operations performed within the totalDonationAmount are in type Double, and that's why the type inference selects it as a Doulbe, and I agree, because the Donation amount might have decimal points")
 
-/*:
- ## Now share your understanding
- 
- 1. Commit your response on this page (Option-Command-C).
- 2. [Add a link][al] to your Computer Science portfolio.
- 
- [al]:
- https://www.youtube.com/watch?v=Wa3Wl3T25jo&list=PLTGGOQnktyWs9TlNJ30pgYgypvIGrt3Lx&index=1
- 
- ### Learning Goals - Programming
- 
- * Goal 2
-    * *Thinking*
-        * I know when to use different data types (integers, floating point, Boolean, strings, et cetera) and data structures and can explain why it is important to use the correct data type or structure.
- 
- [Next](@next)
- */
+
+
+
+
+
+
+
+
 
 
